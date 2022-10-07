8ca94e3620cac21184f20ed78bf1f8d509ce6b6b
Ang Chen Han <angchenhan456@gmail.com>
2022-10-07 18:18:47 +0800

    homework

--- Programming_1/test.cpp
+++ Programming_1/test.cpp
@@ -9,9 +9,8 @@ template <typename Comparable>
 class EquationSolver
 {
 public:
-  Comparable a = 0;
-  Comparable b = PI / 2;
-  //Comparable b = 1;
+  Comparable a;
+  Comparable b;
   Comparable c;
   Comparable d;
   Comparable h;
@@ -22,22 +21,99 @@ public:
   Comparable k; 
   Comparable delta = DBL_EPSILON;
   Comparable eps = DBL_EPSILON;
-  
-  Comparable f(Comparable x)
+
+  void Dom(int y)
   {
-    return(1.0 / x - tan(x));
-    //return(1.0 / x - pow(2,x));
+    switch (y)
+    {
+    case 1:
+      a = 0;
+      b = PI /2;
+      break;
+
+    case 2:
+      a = 0;
+      b = 1;
+      break;
+      
+    case 3:
+      a = 1;
+      b = 3;
+      break;
+
+    case 4:
+      a = 0;
+      b = 4;
+    
+    case 5:
+      c = 4.5;
+
+    case 6:
+      c = 7.7;
+    }
+  }
+  
+  Comparable f(Comparable x , int y)
+  {
+    switch (y)
+    {
+    case 1:
+      return(1.0 / x - tan(x));
+      break;
+
+    case 2:
+      return(1.0 / x - pow(2,x));
+      break;
+      
+    case 3:
+      return(1 / pow(2,x) + exp(x) + 2 * cos(x) - 6);
+      break;
+
+    case 4:
+      return((pow(x,3) + 4 * pow(x,2) + 3 * x + 5) / (2 * pow(x,3) - 9 * pow(x,2) + 18 * x - 2));
+      break;
+
+    case 5:
+      return (tan(x));
+      break;
+      
+    case 6:
+      return (tan(x));
+      break;
+    }
   }
 
-  Comparable fd(Comparable x)
+  Comparable fd(Comparable x , int y)
   {
-    return(-1.0 / (x * x) - pow(1.0 / cos(x) , 2));
-    //return(-1.0 / (x * x) - pow(2,x) * log (2));    
+    switch (y)
+    {
+    case 1:
+      return(-1.0 / (x * x) - pow(1.0 / cos(x) , 2));
+      break;
+
+    case 2:
+      return(-1.0 / (x * x) - pow(2,x) * log (2));
+      break;
+
+    case 3:
+      break;
+
+    case 4:
+      break;
+
+    case 5:
+      return(pow(1.0 / cos(x) , 2));
+      break;
+      
+    case 6:
+      return(pow(1.0 / cos(x) , 2));
+      break;
+    }
   }
 
   int sgn(Comparable x)
   {
-    return(( x > 0) - ( x < 0 ));
+    return((x > 0) - (x < 0));
   } 
 
   Comparable root()
@@ -50,17 +126,18 @@ template <typename Comparable>
 class BisectionMethod : public EquationSolver<double>
 {
 public:
-  void B_Method()
+  void B_Method(int x)
   {
-    if(sgn(f(a)) != sgn(f(b)))
+    Dom(x);
+    if(sgn(f(a , x)) != sgn(f(b , x)))
     { 
       h = b - a;
-      u = f(a);
+      u = f(a , x);
       for(int i=1 ; i<M ; i++)
       {
 	h = h / 2;
 	c = a + h;
-	w = f(c);
+	w = f(c , x);
 	k = i;
 	if((abs(h) < delta) || (abs(w) < eps))
 	{
@@ -80,18 +157,18 @@ template <typename Comparable>
 class NewtonMethod : public EquationSolver<double>
 {
 public:
-  void N_Method()
+  void N_Method(int x)
   {
-    c =  (a + b) / 2;
+    Dom(x);
     for(int i=0 ; i<M ;i++)
     {        
-      u = f(c);
+      u = f(c , x);
       k = i;
       if(abs(u) < eps)
       {
 	break;
       }
-      c = c - u / fd(c);
+      c = c - u / fd(c , x);
     }
   }
 };
@@ -100,12 +177,13 @@ template <typename Comparable>
 class SecantMethod : public EquationSolver<double>
 {
 public:
-  void S_Method()
+  void S_Method(int x)
   {
+    Dom(x);
     c = (a + b) / 3;
     d = (a + b) / 3 * 2; 
-    u = f(c);
-    v = f(d);
+    u = f(c , x);
+    v = f(d , x);
     for(int i=1 ; i<M ; i++)
     {
       if(abs(u) > abs(v))
@@ -121,7 +199,7 @@ public:
       d = c;
       v = u;
       c = c - u * w;
-      u = f(c);
+      u = f(c , x);
       k = i;
       if((abs(c - d) < delta) || (abs(u) < eps))
       {
@@ -136,10 +214,16 @@ int main()
   BisectionMethod<double> A;
   NewtonMethod<double> B;
   SecantMethod<double> C;
-  A.B_Method();
+  A.B_Method(1);
   cout<<A.root()<<endl;
-  B.N_Method();
+  A.B_Method(2);
+  cout<<A.root()<<endl;
+  A.B_Method(3);
+  cout<<A.root()<<endl;
+  A.B_Method(4);
+  cout<<A.root()<<endl;
+  B.N_Method(5);
+  cout<<B.root()<<endl;;
+  B.N_Method(6);
   cout<<B.root()<<endl;
-  C.S_Method();
-  cout<<C.root()<<endl;
 }
